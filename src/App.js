2133cbfc95cb4a520ea0813f4e3b3524357b973e
saiyerniakhil <saiyerniakhil>
2019-04-19 12:39:22 +0530

    Intial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+//import ReactDOM from 'react-dom'
+import { Button } from 'reactstrap';
 
-class App extends Component {
+class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter : 0
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  handleClick(event) {
+    if(event.target.className === 'plus') {
+      this.setState({
+        counter : this.state.counter + 1
+      }) 
+    }
+    else if (event.target.className === 'minus') {
+      this.setState({
+        counter : this.state.counter - 1
+      }) 
+    }
+    else {
+      this.setState({
+        counter : 0
+      }) 
+    }
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return(
+      <div>
+        <div style={{backgroundColor:"aliceblue",width:300,height:200}}>
+          <h1>React Clicker!</h1>
+          <h1>{this.state.counter}</h1>
+          <div><button className = 'plus' onClick={this.handleClick} >+</button> <button className='reset' onClick={this.handleClick}>Reset</button> <button className='minus' onClick={this.handleClick}>-</button> </div>
+        </div>
+      </div>  
     );
   }
 }
